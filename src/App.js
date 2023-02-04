c91ba8fcbed5fb47994d634b81b98509e75a4806
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-04 16:01:13 +0200

    Fix
    fix filename, add label

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      
+    <div> 
+
       <LoginForm />
     </div>
   );
